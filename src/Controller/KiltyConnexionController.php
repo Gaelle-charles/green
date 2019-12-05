a7483e91cd1714b622947bedbaf553fabe53e232
ANTHONY-ROSMAEL <54911861+ANTHONY-ROSMAEL@users.noreply.github.com>
2019-12-05 14:49:08 -0400

    inscription

---
+++ src/Controller/KiltyConnexionController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Controller;
+use App\Entity\Trader;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+/**
+ * Class KiltyConnexionController
+ * @package App\Controller
+ */
+class KiltyConnexionController extends AbstractController
+{
+
+
+    /**
+     * @Route("/boutique", name="shop_home", methods={"GET"})
+     */
+    public function register()
+    {
+
+        $trader = new Trader();
+        $trader->setRoles(['ROLE_MEMBRE']);
+
+
+
+        $form = $this->createFormBuilder($trader)
+            ->add('firstname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Je m'inscris !",
+                'attr' => [
+                    'class' => 'btn btn-block btn-dark'
+                ]
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+
+    }
+
+}
