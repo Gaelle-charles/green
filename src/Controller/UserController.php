72928334f7517833f70c17d15de8070977ecc0f9
ANTHONY-ROSMAEL <54911861+ANTHONY-ROSMAEL@users.noreply.github.com>
2019-12-06 14:33:19 -0400

    modif

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 /**
@@ -100,5 +101,36 @@ class UserController extends AbstractController
         ]);
 
     }
+
+
+    /**
+     * @Route("/login", name="shop_login", methods={"GET|POST"})
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login( AuthenticationUtils $authenticationUtils ):Response
+    { //
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('shop/user/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/deconnexion.html", name="app_logout")
+     * @throws \Exception
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+
+
+
+
+
 
     }
